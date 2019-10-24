55d12ce8f5c4fbcf9f50d6a26d01b181e747bd82
Alexander Swanson <aswanson076@gmail.com>
2019-10-24 11:06:10 -0400

    Finished deliverable 2, removed errors from createPairs, and updated finalData.txt

--- Deliverable 1/createPairs.py
+++ Deliverable 1/createPairs.py
@@ -15,15 +15,21 @@ for i in emailList:
     for j in emailList:
         if i[0] == j[0] and i[1] != j[1]:
             pairlist.append(i[1].rstrip()+":"+j[1])
+print("pairlist size: "+str(len(pairlist)))
 
 #remove duplicates and reverse duplicates
+#neo4j only supports diected edges, so we need to create the reverse of all pairs
+#this is inefficient and might have memory constraints on 32 bit python
 finallist = []
 for i in pairlist:
     if i not in finallist:
         temp = i.split(":")
-        newi = (temp[1]+":"+temp[0]).rstrip()+"\n"
+        newi = (temp[1].rstrip()+":"+temp[0].rstrip())+"\n"
         if newi not in finallist:
             finallist.append(i)
+            finallist.append(newi)
+print("finallist size: "+str(len(finallist)))
+
 
 #writes data to file
 with open('finalData.txt', 'w') as f:
